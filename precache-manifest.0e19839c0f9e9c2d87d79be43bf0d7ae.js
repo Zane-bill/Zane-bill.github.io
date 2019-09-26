02b2e9b354eaff42ba54f5a1d24499edc5b58f4a
张标 <zhangbiao@wuzhenpay.com>
2019-09-26 13:38:24 +0800

    0926

---
+++ precache-manifest.0e19839c0f9e9c2d87d79be43bf0d7ae.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "/fonts/ionicons.143146fa.woff2"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "fc9d53ad41f1254f04fd",
+    "url": "/css/app.d96148c2.css"
+  },
+  {
+    "revision": "d47271be3dbe7ac7da75",
+    "url": "/css/chunk-vendors.177f70bf.css"
+  },
+  {
+    "revision": "d47271be3dbe7ac7da75",
+    "url": "/js/chunk-vendors.fb83911e.js"
+  },
+  {
+    "revision": "caecbb8ea22654010ce9",
+    "url": "/js/about.d9f1effb.js"
+  },
+  {
+    "revision": "fc9d53ad41f1254f04fd",
+    "url": "/js/app.df82afbc.js"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "042ac51fd9cdd217050ec403a024ecd3",
+    "url": "/img/please.042ac51f.jpg"
+  },
+  {
+    "revision": "935bc8e843f8319d2605083e2a7d62b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caecbb8ea22654010ce9",
+    "url": "/css/about.ba8e9944.css"
+  }
+];
